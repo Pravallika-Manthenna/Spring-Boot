e7411db8a26cd57692046665c5696c7456ea4886
Pravallika-Manthenna <mslpravallika6@gmail.com>
2019-07-24 13:30:28 +0530

    task4 swagger

---
+++ src/main/java/com/stackroute/Config/SwaggerConfig.java
@@ -0,0 +1,22 @@
+package com.stackroute.Config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+
+public class SwaggerConfig {
+
+    @Bean
+    public Docket productApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.stackroute.Config"))
+                //.paths(regex("/api/v1.*"))
+                .build();
+    }
+}
