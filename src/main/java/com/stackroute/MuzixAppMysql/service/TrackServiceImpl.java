2bd3cfb2a81f0111877334b7f486e70f3e3d84df
Pravallika-Manthenna <51950359+Pravallika-Manthenna@users.noreply.github.com>
2019-08-08 10:14:50 +0530

    Update TrackServiceImpl.java

--- src/main/java/com/stackroute/MuzixAppMysql/service/TrackServiceImpl.java
+++ src/main/java/com/stackroute/MuzixAppMysql/service/TrackServiceImpl.java
@@ -14,7 +14,7 @@ import java.util.Optional;
 @Service
 public class TrackServiceImpl implements TrackService{
 
-    TrackRepository trackRepository;
+   private TrackRepository trackRepository;
 
     //autowired annotation is used 
     @Autowired
